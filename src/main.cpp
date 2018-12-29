d3aa363f31cd3e5ce73400ba6cafd79c9d57279c
sotsoguk <sotsoguk@gmail.com>
2018-12-29 02:23:57 +0100

    1

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,10 @@ using namespace std::complex_literals;
 
 int main() {
     // setup range for mandelbrot a.k.a. zoom
-    const complex<double> z_ll = 0.270085 + 0.004440i;
-    const complex<double> z_ur = 0.271000 + 0.004810i;
+    //const complex<double> z_ll = 0.270085 + 0.004440i;
+    //const complex<double> z_ur = 0.271000 + 0.004810i;
+    const complex<double> z_ll = -2.0 -1.2i;
+    const complex<double> z_ur = 1.0 + +1.2i;
 
     const double xmin = z_ll.real();
     const double xmax = z_ur.real();
@@ -20,10 +22,11 @@ int main() {
     const double ymax = z_ur.imag(); 
 
     // basic setup for image output
-    const int nx = 1024;
+    const int nx = 1200;
     //const int ny = 100;
     const int ny = (nx * (ymax-ymin))/ (xmax - xmin);
-    const unsigned int maxIter = 100;
+    const unsigned int maxIter = 1000;
+    const unsigned int halfMaxIter = maxIter / 2;
 
     double dx = (xmax-xmin) / double(nx);
     double dy = (ymax-ymin) / double(ny);
@@ -42,21 +45,28 @@ int main() {
             c = z_ll + x*dx + y*dy*1i;
             complex<double> z = c;
             bool isInside = true;
+            int numToInfinity = 0;
             //check if series converges
             for (unsigned int n=0; n<maxIter; n++){
                 z = z*z +c;
                 if (abs(z)>2){
                     isInside = false;
+                    numToInfinity = n;
                     break;
                 } 
             }
             int ir =0;
             int ig = 0;
             int ib = 0;
-            if (isInside){
-                ir = 255;
-                ig = 0;
-                ib = 0;
+            if (!isInside){
+                if (numToInfinity < halfMaxIter){
+                    ib = int(255.99*numToInfinity / halfMaxIter);
+                }
+                else{
+                    ib = 255;
+                    ig = int(255.99 * (numToInfinity-halfMaxIter)/halfMaxIter);
+                    ir = ig;
+                }
             }
             imgData[(nx*numChannels)*y + numChannels*x + 0] = ir;
             imgData[(nx*numChannels)*y + numChannels*x + 1] = ig;
